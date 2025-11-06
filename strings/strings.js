2e3f43e8c4c4a1d27cb1ac1962724b5c3b41feed
Isagba gerare <gerardisagba17@gmail.com>
2025-11-06 23:00:00 +0100

    completed exercise on strings.js

--- strings/strings.js
+++ strings/strings.js
@@ -6,7 +6,7 @@
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let newString
+let newString = "this is my first new string"
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -22,7 +22,7 @@ let getMyDataType = "This is a string"
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let gotDataType
+let gotDataType = typeof getMyDataType
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -38,7 +38,7 @@ let getMyLenght = "Get the length of this string"
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let myLength
+let myLength = getMyLenght.length
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -57,7 +57,7 @@ let concat3 = "father!"
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let concatStrings
+let concatStrings = concat1 + concat2 + concat3;
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -75,7 +75,7 @@ let subString = "out"
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let stringContains
+let stringContains= mainString.includes(subString)
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -93,7 +93,7 @@ let subStringIgnoreCase = "friends"
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let stringContainsIgnoreCase
+let stringContainsIgnoreCase = mainStringIgnoreCase.toLowerCase().includes(subStringIgnoreCase.toLowerCase());
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -109,7 +109,7 @@ let getFirstCharacter = "You need my first character"
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let firstCharacter
+let firstCharacter = getFirstCharacter.charAt(0);
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -125,7 +125,7 @@ let getFirstFourCharacters = "You need my first four characters"
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let firstFourCharacters
+let firstFourCharacters = getFirstFourCharacters.substring(0,4);
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -140,8 +140,8 @@ let firstFourCharacters
 let getLastCharacter = "You need my last character"
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
-
-let lastCharacter
+let lastindex = getLastCharacter.length - 1;
+let lastCharacter = getLastCharacter.charAt(lastindex);
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -156,8 +156,8 @@ let lastCharacter
 let getLastSixCharacters = "You need my last six characters"
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
-
-let lastSixCharacters
+let lastsixindex = getLastSixCharacters.length - 6;
+let lastSixCharacters = getLastSixCharacters.substring(lastsixindex);
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -175,7 +175,7 @@ let checkEquality2 = "Are we equal?"
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let stringsAreEqual
+let stringsAreEqual = checkEquality1 === checkEquality2;
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -193,7 +193,7 @@ let checkEqualityIgnoreCase2 = "are we EQUAL?"
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let stringsAreEqualIgnoreCase
+let stringsAreEqualIgnoreCase = checkEqualityIgnoreCase1.toUpperCase() === checkEqualityIgnoreCase2.toUpperCase();
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -209,7 +209,7 @@ let replaceMe = "John just moved to Montreal. John will be meeting his roomate s
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let replacedString
+let replacedString = replaceMe.replace(/John/g, "Michael");
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -226,7 +226,7 @@ let removeMe = "We will going to be leaving at noon sharp."
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let removedString
+let removedString = removeMe.replace("going to ", "");
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -243,8 +243,9 @@ let targetString1 = "THE-QUICK-BROWN-FOX-JUMPS-OVER-THE-LAZY-DOG"
 //  NOTE: Only add functions to testString1 and do not create any new variables
 //  The tests will check if testString1 is equal to targetString1
 //
+//
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
-
+testString1=testString1.toUpperCase().replace(/ /g, "-");
 
 
 ////////////////////////////////////////////////////////////////////////
@@ -264,7 +265,7 @@ let targetString2 = "i am the second best, you are the first best!"
 //  Hint: There is more than one way to solve this - be creative!
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
-
+testString2 = testString2.toLowerCase().replace("first sentence." , "second best,").replace("second sentence", "first best!");
 
 
 ////////////////////////////////////////////////////////////////////////
@@ -288,7 +289,7 @@ let escapedString
 // escapedString = "My professor said: \"Study hard and you will succeed\" \nI replied: \"Thanks for the tip!\""
 
 ////////////////////////////////////////////////////////////////////////
-
+escapedString = "My professor said: \"Study hard and you will succeed\" \nI replied: \"Thanks for the tip!\""
 
 
 /////////////////////// END OF STRINGS EXERCISES ///////////////////////
